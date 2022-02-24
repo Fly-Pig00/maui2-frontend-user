130c718952ea1255e63705b8ab7b8b417230501c
marlborocool <greatrj890106@gmail.com>
2022-02-24 19:06:24 +0900

    Update backend route

--- src/appConfig.js
+++ src/appConfig.js
@@ -1,5 +1,5 @@
 export const appConfig = {
-  apiUrl: process.env.REACT_APP_API_URL || 'https://secure-falls-43253.herokuapp.com/api',
+  apiUrl: process.env.REACT_APP_API_URL || 'https://api.mauibank.io/api',
   // apiUrl: process.env.REACT_APP_API_URL || 'http://localhost:4000/api',
   MICRO: 1_000_000,
   BLOCKSPERYEAR: 4_656_810,
