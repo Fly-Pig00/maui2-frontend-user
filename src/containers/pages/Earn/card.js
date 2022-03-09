d147dfe9675a0811ac552080ed6e8851a32df477
marlborocool <greatrj890106@gmail.com>
2022-03-09 16:15:53 +0900

    Add earn->deposit tooltip

--- src/containers/pages/Earn/card.js
+++ src/containers/pages/Earn/card.js
@@ -1,17 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import { useForm } from "react-hook-form";
-import InputAmount from '../../../components/Form/InputAmount';
-import { unmaskCurrency } from '../../../utils/masks';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
 import { toast } from "react-toastify";
 import { Dec } from "@terra-money/terra.js";
-import { apiEarnDeposit } from '../../../saga/actions/workflow';
+import { Popover } from 'react-tiny-popover';
+
 import Button from '../../../components/Button';
-import { appConfig } from '../../../appConfig';
+import InputAmount from '../../../components/Form/InputAmount';
 import AgreeWithCheckbox from '../../../components/Form/AgreeWithCheckbox';
+import { unmaskCurrency } from '../../../utils/masks';
+import { apiEarnDeposit } from '../../../saga/actions/workflow';
+import { appConfig } from '../../../appConfig';
 
 function Cards (props) {
+  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
 	// get functions to build form with useForm() hook
   const hookForm = useForm();
 	const { handleSubmit, setValue } = hookForm;
@@ -49,6 +52,10 @@ function Cards (props) {
     return null;
   }
 
+  function handleDepositHelpClick(e) {
+    setIsPopoverOpen(!isPopoverOpen);
+  }
+
   // handle functions
 	const onSubmit = (data) => {
     if (!props.workflow.isLogged) {
@@ -82,7 +89,26 @@ function Cards (props) {
   return (
     <form name={props.name} id={props.name} className='w-full bg-earn-card rounded-[40px] p-[30px] pl-[50px] border dark:border-[#FFFFFF30]' onSubmit={handleSubmit(onSubmit)}>
       { props.isDeposit ?
-        <div className=' font-semibold text-[24px] text-[#273855] dark:text-[#F9D3B4]'>Enter an amount for 15% APY <span className='text-[#745FF2]'>Deposit</span></div>
+        <div className=' font-semibold text-[24px] text-[#273855] dark:text-[#F9D3B4]'>Enter an amount for 15% APY
+          <Popover
+            containerClassName='z-[999999] pl-[20px]'
+            isOpen={isPopoverOpen}
+            positions={['top']} // preferred positions by priority
+            align={'center'}
+            padding={2}
+            onClickOutside={() => setIsPopoverOpen(false)}
+            content={
+              <>
+                <div className='rounded-md border border-[#00214732] p-5 bg-[#ffffff] m-auto max-w-[90%] md:max-w-[600px] max-h-[500px] overflow-y-auto text-[#3f556e] text-[14px] scrollbar'>
+                  <p>When you deposit your money for 15% APY deposit, we deduct 4.167% for Maui fee to get it ensured transaction with Anchor protocol for the first 15 days, from when you can withdraw your original deposit freely.</p>
+                </div>
+                <span className='arrow left-2 hidden md:block' />
+              </>
+            }
+          >
+            <span className='ml-[10px] text-[#745FF2] cursor-pointer underline' onClick={handleDepositHelpClick}>Deposit</span>
+          </Popover>
+        </div>
       :
         <div className=' font-semibold text-[24px] text-[#273855] dark:text-[#F9D3B4]'>How much would you like to <span className='text-transparent bg-clip-text bg-gradient-to-b from-[#FF0000FF] to-[#FF000010]'>Withdraw</span> ?</div>
       }
